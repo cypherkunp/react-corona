4fe0155d62797d1ad294531f13ed24069e16142c
devvratshukla <devvratshukla@gmail.com>
2020-05-09 06:19:20 +0000

    Deploying to gh-pages from  @ cae8f43219082cb39525a618c219a3efcee5391e 🚀

---
+++ precache-manifest.2ff571d6198e9cc0c1d97504978a7fd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6abf8e910c4255cd67e99acb6fee54b7",
+    "url": "/react-corona/index.html"
+  },
+  {
+    "revision": "26c18d00667f531c9223",
+    "url": "/react-corona/static/css/main.0448a933.chunk.css"
+  },
+  {
+    "revision": "b0ba8c096e40e1862f50",
+    "url": "/react-corona/static/js/2.601de899.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-corona/static/js/2.601de899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c18d00667f531c9223",
+    "url": "/react-corona/static/js/main.2a33c13f.chunk.js"
+  },
+  {
+    "revision": "a9545755f5db3b9c9fc6",
+    "url": "/react-corona/static/js/runtime-main.211f4034.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-corona/static/media/header-image.d7265326.png"
+  }
+]);
